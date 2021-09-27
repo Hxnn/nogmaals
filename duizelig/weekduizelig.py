fefc83dbb27d3eff22fb31b7d620b1f3d0669a7e
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-09-27 14:04:53 +0200

    Added 30-0 and quit file

--- duizelig/weekduizelig.py
+++ duizelig/weekduizelig.py
@@ -1,1 +1,20 @@
-input('Which day of the week do you choose?')
+while True:
+    day = input('Kies een dag van de week, ma, di, wo, do, vr, za, zo. ')
+
+    if day == "ma":
+        print('ma')
+    elif day == "di":
+        print("ma, di")
+    elif day == "wo":
+        print("ma, di, wo")
+    elif day == "do":
+        print("ma, di, wo, do")
+    elif day == "vr":
+        print("ma, di, wo, do, vr")
+    elif day == "za":
+        print("ma, di, wo, do, vr, za")
+    elif day == "zo":
+        print("ma, di, wo, do, vr, za, zo")
+    else:
+        print("Dit was geen optie!")
+    break
