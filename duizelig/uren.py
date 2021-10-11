cfa7014456f9dfbc2908bfecb7441bd6869351f9
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-10-11 10:56:09 +0200

    Commited right file

--- duizelig/uren.py
+++ duizelig/uren.py
@@ -1,28 +1,27 @@
 dt = 13
 start = 1
-AM = "AM"
-PM = "PM"
-while start < dt:
-    print(start, AM)
-    start = start + 1
-
 t = 13
 startt = 1
-while start < t:
-    print(start, PM)
-    startt = start + 1
+AM = "AM"
+PM = "PM"
+while start and startt < 13:
+    if start < dt:
+        print(start, AM)
+        start += 1
+    else:
+        print(startt, PM)
+        startt += 1
 
-# 2nd option:
 # dt = 13
 # start = 1
 # t = 13
-# startt = 1
+# startt = 13
 # AM = "AM"
 # PM = "PM"
-# while start and startt < 13:
-#     if start < dt:
-#         print(start, AM)
-#         start = start + 1
-#     else:
-#         print(startt, PM)
-#         startt = startt + 1
+# while start < dt:
+#     print(start, AM)
+#     start += 1
+#
+# while startt < t:
+#     print(startt, PM)
+#     startt += 1
